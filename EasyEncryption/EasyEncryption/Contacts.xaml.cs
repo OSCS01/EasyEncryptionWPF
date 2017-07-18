95b01415c47385bfcd752b4211b2199e82549f66
peter_000 <peter_000@CEPHAS>
2017-07-18 14:05:43 +0800

    g

--- EasyEncryption/EasyEncryption/Contacts.xaml.cs
+++ EasyEncryption/EasyEncryption/Contacts.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +21,39 @@ namespace EasyEncryption
     /// </summary>
     public partial class Contacts : Window
     {
+        const string constring = @"Data Source=CEPHAS\SQLEXPRESS;Initial Catalog = EasyEncryption;Integrated Security = True";
         public Contacts()
         {
             InitializeComponent();
+            LoadContacts();
+        }
+
+        private void LoadContacts()
+        {
+            SqlConnection con = new SqlConnection(constring);
+            try
+            {
+                con.Open();
+                string query = "SELECT name FROM Users";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.ExecuteNonQuery();
+
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable("Users");
+                sda.Fill(dt);
+                ContactsGrid.ItemsSource = dt.DefaultView;
+                sda.Update(dt);
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
